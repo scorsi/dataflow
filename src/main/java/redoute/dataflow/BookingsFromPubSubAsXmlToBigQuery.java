cc6702e842e560fbd335f185ca05acdbba1b50e6
scorsi <sylvain.corsini@gmail.com>
2017-12-29 16:11:09 +0100

    Get XML as String and parse it to Booking Object

--- src/main/java/redoute/dataflow/BookingsFromPubSubAsXmlToBigQuery.java
+++ src/main/java/redoute/dataflow/BookingsFromPubSubAsXmlToBigQuery.java
@@ -8,6 +8,11 @@ import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.windowing.SlidingWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.joda.time.Duration;
+import redoute.dataflow.data.Booking;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
 
 public class BookingsFromPubSubAsXmlToBigQuery {
 
@@ -30,7 +35,33 @@ public class BookingsFromPubSubAsXmlToBigQuery {
                     @SuppressWarnings("unused")
                     @ProcessElement
                     public void processElement(ProcessContext c) {
-                        System.out.println(c.element());
+                        System.out.println("String: " + c.element());
+                        c.output(c.element());
+                    }
+                }))
+
+                .apply("Transform XML input to Booking Object", ParDo.of(new DoFn<String, Booking>() {
+                    @SuppressWarnings("unused")
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                        try {
+                            JAXBContext jaxbContext = JAXBContext.newInstance(Booking.class);
+                            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+                            StringReader reader = new StringReader(c.element());
+
+                            Booking b = (Booking) unmarshaller.unmarshal(reader);
+                            c.output(b);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }))
+
+                .apply("Display content", ParDo.of(new DoFn<Booking, String>() {
+                    @SuppressWarnings("unused")
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                        System.out.println("Object: " + c.element().toString());
                     }
                 }));
 
